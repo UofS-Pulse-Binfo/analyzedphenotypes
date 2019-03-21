33edc37f713651317eee4f8a560d18d0f83d93dd
Reynold Tan <reynoldltan@gmail.com>
2019-03-21 20:49:42 +0000

    Bug fixed in review

--- includes/TripalFields/local__magnitude_of_phenotypes/theme/script.js
+++ includes/TripalFields/local__magnitude_of_phenotypes/theme/script.js
@@ -6,27 +6,71 @@
 Drupal.behaviors.MagnitudeOfPhenotypes = {
   attach: function(context, settings) {
   
-  $('#magnitude-of-phenotypes-btn-reveal div').click(function() {
-    var h, classRem, classAdd;
-    var phenotypes = $('#field-magnitude-of-phenotypes-wrapper-phenotypes');
-    // Default height of the container.
-    var minHeight = 250;
+  // Wrapper.
+  var wrapper = {};
+  wrapper.id  = '#field-magnitude-of-phenotypes-wrapper-phenotypes';
+  wrapper.btn   = '#magnitude-of-phenotypes-btn-reveal';
+  wrapper.listId  = '#list-magnitude-of-phenotypes-phenotypes';
+  wrapper.minHeight = 220;
+  
+  var listHeight = 0;
 
-    if (phenotypes.height() == minHeight) {
-      h = phenotypes[0].scrollHeight;
-      classRem = 'on';
-      classAdd = 'off';
+  // Phenotypes pane launched.
+  $('a').click(function() {
+    if ($(wrapper.id) && $(wrapper.id).is(':visible')) {
+      listHeight = $(wrapper.listId).height();
+      btnReveal();
     }
-    else {
-      h = minHeight + 'px';
-      classRem = 'off';
-      classAdd = 'on';
-    }
-
-    phenotypes.css('max-height', h);
-
-    $(this)
-      .removeClass('magnitude-of-phenotypes-phenotypes-reveal-' + classRem)
-      .addClass('magnitude-of-phenotypes-phenotypes-reveal-' + classAdd);
   });
+
+  //
+  if ($(wrapper.id)) {
+    // List of phenotypes enabled.
+    $(window).resize(function() { 
+      listHeight = $(wrapper.listId).height();
+      btnReveal();
+    });
+
+
+    // Attach listener to reveal/show less button.
+    $(wrapper.btn + ' div').click(function() {
+      var h, classRem, classAdd;
+      var phenotypes = $(wrapper.id);
+      // Default height of the container.
+      var minHeight = 250;
+ 
+      if (phenotypes.height() == minHeight) {
+        h = phenotypes[0].scrollHeight;
+        classRem = 'on';
+        classAdd = 'off';
+      }
+      else {
+        h = minHeight + 'px';
+        classRem = 'off';
+        classAdd = 'on';
+      }
+
+      phenotypes.css('max-height', h);
+
+      $(this)
+        .removeClass('magnitude-of-phenotypes-phenotypes-reveal-' + classRem)
+        .addClass('magnitude-of-phenotypes-phenotypes-reveal-' + classAdd);
+    });
+    
+    
+    /**
+     * Function toggle reveal button.
+     */
+    function btnReveal() {
+      var btn = $(wrapper.btn);
+      
+      if (listHeight > wrapper.minHeight) {
+        // Overflow detected.
+        btn.show();
+      } else {
+        // All shown.
+        btn.hide();
+      }
+    }
+  }
 }}}(jQuery));
