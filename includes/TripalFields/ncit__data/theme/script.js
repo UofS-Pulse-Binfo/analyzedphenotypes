f5b6ba0044423256a7e4ac5409300e081c17439d
Reynold Tan <reynold@rain.baskt.bioinfo>
2019-06-05 14:38:17 -0600

    Working field: todo - document code and test in another clone

--- includes/TripalFields/ncit__data/theme/script.js
+++ includes/TripalFields/ncit__data/theme/script.js
@@ -10,4 +10,48 @@ Drupal.behaviors.NCITData = {
   $('#ap-search-autocomplete-textfield').click(function() {
     $(this).select();
   });
+
+  var wrapper = {};
+  wrapper.main = '#ap-ncitdata-table-summary-wrapper';
+  wrapper.btn  = '#ap-ncitdata-btn-reveal';
+
+  // Attach listener to reveal/show less button.
+  $(wrapper.btn + ' div').click(function() {
+    var h, classRem, classAdd;
+    var main = $(wrapper.main);
+
+    // Default height of the table container.
+    var minHeight = 300;
+
+    if (main.height() == minHeight) {
+      h = main[0].scrollHeight;
+      classRem = 'on';
+      classAdd = 'off';
+    }
+    else {
+      h = minHeight + 'px';
+      classRem = 'off';
+      classAdd = 'on';
+    }
+
+    main.css('max-height', h);
+
+    $(this)
+      .removeClass('ap-ncitdata-table-row-reveal-' + classRem)
+      .addClass('ap-ncitdata-table-row-reveal-' + classAdd);
+  });
+
+
+  // Summary information tool tip.
+  $('#ap-summary-overview-list ul li').mouseover(function() {
+    var pos = $(this).position();
+
+    $(this).find('.ap-tooltip').show().css({
+      'top': pos.top + 40,
+      'left' : pos.left
+    });
+  })
+  .mouseleave(function() {
+    $('.ap-tooltip').hide();
+  });
 }}}(jQuery));
