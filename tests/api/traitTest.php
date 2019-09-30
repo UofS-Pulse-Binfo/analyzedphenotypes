2bdc070075b2ab0649e204f28b239326435a4fd1
Lacey Sanderson <lacey.sanderson@usask.ca>
2019-09-30 16:00:05 -0600

    Improve Trait tests.

--- tests/api/traitTest.php
+++ tests/api/traitTest.php
@@ -57,6 +57,207 @@ class traitTest extends TripalTestCase {
   }
 
   /**
+   * Test ap_get_trait().
+   *
+   * @group api
+   * @group trait
+   *
+   * @dataProvider provideTraits
+   */
+  public function testApGetTrait($trait_details, $expected_pass) {
+
+    // Supress tripal errors
+    putenv("TRIPAL_SUPPRESS_ERRORS=TRUE");
+
+    // Set the line in the config page for the fake organism.
+    // Variables are analyzedphenotypes_systemvar_[genus]_[cv/db/ontology]
+    $genus = 'Tripalus-AP-TESTSUITE';
+    $trait_cv = factory('chado.cv')->create();
+    variable_set('analyzedphenotypes_systemvar_tripalus-ap-testsuite_cv', $trait_cv->cv_id);
+    $trait_db = factory('chado.db')->create();
+    variable_set('analyzedphenotypes_systemvar_tripalus-ap-testsuite_db', $trait_db->db_id);
+
+    // Try to retrieve a trait that doesn't yet exist.
+    $trait = ap_get_trait($genus, $trait_details['name']);
+    $this->assertFalse($trait,
+      "Tried to get a trait which doesn't exist.");
+
+    // Try to retrieve a trait that we just inserted.
+    ap_insert_trait($trait_details);
+    $trait = ap_get_trait($genus, $trait_details['name']);
+    $this->assertNotFalse($trait);
+
+    // Check that we have the basics of a trait.
+    $this->assertNotEquals(0,$trait->cvterm_id, 'There was no cvterm_id');
+    $this->assertEquals($trait_details['name'],$trait->name,
+      'The retrieved trait name did not match.');
+    $this->assertEquals($trait_details['description'],$trait->definition,
+      'The retrieved trait definition did not match.');
+
+    // Remove the configuration.
+    variable_del('analyzedphenotypes_systemvar_'.$genus.'_cv');
+    variable_del('analyzedphenotypes_systemvar_'.$genus.'_db');
+
+    // Clean the buffer and unset tripal errors suppression
+    putenv("TRIPAL_SUPPRESS_ERRORS");
+
+  }
+
+  /**
+   * Test ap_get_method().
+   *
+   * @group api
+   * @group trait
+   *
+   * @dataProvider provideTraits
+   */
+  public function testApGetMethod($trait_details, $expected_pass) {
+    $genus = 'Tripalus-AP-TESTSUITE';
+
+    $this->configureApForOrganism();
+
+    // Try to retrieve a method that doesn't yet exist.
+    $method = ap_get_method($genus, $trait_details['method_title']);
+    $this->assertFalse($method,
+      "Tried to get a method which doesn't exist.");
+
+    // Try to retrieve a method that we just inserted.
+    $trait = ap_insert_trait($trait_details);
+    $this->assertNotFalse($trait, 'not even the trait!?!?');
+    $method = ap_get_method($genus, $trait_details['method_title']);
+    $this->assertNotFalse($method,
+      "Unable to retrieve a method we just inserted.");
+
+    // Check that we have the basics of a method.
+    $this->assertNotEquals(0,$method->cvterm_id, 'There was no cvterm_id');
+    $this->assertEquals($trait_details['method_title'],$method->name,
+      'The retrieved trait name did not match.');
+    $this->assertEquals($trait_details['method'],$method->definition,
+      'The retrieved trait definition did not match.');
+
+  }
+
+  /**
+   * Test ap_get_trait_methods.
+   *
+   * @group api
+   * @group trait
+   *
+   * @dataProvider provideTraits
+   */
+  public function testApGetTraitMethods($trait_details, $expected_pass) {
+    $genus = 'Tripalus-AP-TESTSUITE';
+
+    $this->configureApForOrganism();
+
+    // Try to retrieve methods for a trait that doesn't yet exist.
+    $methods = ap_get_trait_methods($genus, $trait_details['name']);
+    $this->assertFalse($methods,
+      "Tried to get methods for a trait which doesn't exist.");
+
+    // Try to retrieve methods for a trait that we just inserted.
+    $trait = ap_insert_trait($trait_details);
+    $this->assertNotFalse($trait, 'not even the trait!?!?');
+    $methods = ap_get_trait_methods($genus, $trait_details['name']);
+    $this->assertNotFalse($methods,
+      "Unable to retrieve methods for the trait we just inserted.");
+    $this->assertEquals(1, sizeof($methods), 'There should only be one method.');
+  }
+
+  /**
+   * Test ap_get_method_units.
+   *
+   * @group api
+   * @group trait
+   *
+   * @dataProvider provideTraits
+   */
+  public function testApGetMethodUnits($trait_details, $expected_pass) {
+    $genus = 'Tripalus-AP-TESTSUITE';
+
+    $this->configureApForOrganism();
+
+    // Try to retrieve methods for a trait that doesn't yet exist.
+    $units = ap_get_trait_methods($genus, $trait_details['method']);
+    $this->assertFalse($units,
+      "Tried to get units for a method which doesn't exist.");
+
+    // Try to retrieve methods for a trait that we just inserted.
+    $trait = ap_insert_trait($trait_details);
+    $this->assertNotFalse($trait, 'not even the trait!?!?');
+    $units = ap_get_method_units($genus, $trait_details['method']);
+    $this->assertNotFalse($units,
+      "Unable to retrieve units for the trait we just inserted.");
+    $this->assertEquals(1, sizeof($units), 'There should only be one unit.');
+  }
+
+  /**
+   * Test ap_get_method_id.
+   *
+   * @group api
+   * @group trait
+   *
+   * @dataProvider provideTraits
+   */
+  public function testApGetMethodID($trait_details, $expected_pass) {
+    $genus = 'Tripalus-AP-TESTSUITE';
+
+    $this->configureApForOrganism();
+
+    // Try to retrieve methods for a trait that doesn't yet exist.
+    $id = ap_get_method_id($genus, $trait_details['method']);
+    $this->assertFalse($id,
+      "Tried to get method id for a method which doesn't exist.");
+
+    // Try to retrieve methods for a trait that we just inserted.
+    $trait = ap_insert_trait($trait_details);
+    $this->assertNotFalse($trait, 'not even the trait!?!?');
+    $id = ap_get_method_id($genus, $trait_details['method']);
+    $this->assertNotFalse($id,
+      "Unable to retrieve  method id for the method we just inserted.");
+  }
+
+  /**
+   * Test ap_get_unit_id.
+   *
+   * @group api
+   * @group trait
+   *
+   * @dataProvider provideTraits
+   */
+  public function testApGetUnitID($trait_details, $expected_pass) {
+    $genus = 'Tripalus-AP-TESTSUITE';
+
+    $this->configureApForOrganism();
+
+    // Try to retrieve units for a trait that doesn't yet exist.
+    $id = ap_get_unit_id($genus, $trait_details['unit']);
+    $this->assertFalse($id,
+      "Tried to get unit id for a unit which doesn't exist.");
+
+    // Try to retrieve units for a trait that we just inserted.
+    $trait = ap_insert_trait($trait_details);
+    $this->assertNotFalse($trait, 'not even the trait!?!?');
+    $id = ap_get_unit_id($genus, $trait_details['unit']);
+    $this->assertNotFalse($id,
+      "Unable to retrieve  unit id for the unit we just inserted.");
+  }
+
+  /**
+   * Configure the system variables for Tripalus-AP-TESTSUITE.
+   */
+  public function configureApForOrganism() {
+    $trait_cv = factory('chado.cv')->create();
+    variable_set('analyzedphenotypes_systemvar_tripalus-ap-testsuite_cv', $trait_cv->cv_id);
+    $method_cv = factory('chado.cv')->create();
+    variable_set('analyzedphenotypes_systemvar_tripalus-ap-testsuite_method', $method_cv->cv_id);
+    $unit_cv = factory('chado.cv')->create();
+    variable_set('analyzedphenotypes_systemvar_tripalus-ap-testsuite_unit', $unit_cv->cv_id);
+    $trait_db = factory('chado.db')->create();
+    variable_set('analyzedphenotypes_systemvar_tripalus-ap-testsuite_db', $trait_db->db_id);
+  }
+
+  /**
    * Data Provider
    */
   public function provideTraits() {
@@ -64,7 +265,7 @@ class traitTest extends TripalTestCase {
     $traits = [];
 
     // Prepare our necessary records.
-    $organism = factory('chado.organism')->create();
+    $organism = factory('chado.organism')->create(['genus' => 'Tripalus-AP-TESTSUITE']);
 
     // Non-existent, should work.
     $traits[] = [
